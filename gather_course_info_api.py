0e17621f5f43946f6fd8782fb47b499a9f4a143e
Hongtao Xiao <hong@Hongtaos-MacBook-Pro.local>
2018-06-14 20:17:20 -0300

    use pandas to gather result and write to file

--- gather_course_info_api.py
+++ gather_course_info_api.py
@@ -1,7 +1,7 @@
 import requests
 import math
 import time
-
+import pandas as pd
 
 def url():
     prefix = "https://www.udemy.com"
@@ -25,13 +25,19 @@ def price_number(price_text):
     number = price_text.replace('CA$', '')
     return number
 
+def append_course_info(info_list, course):
+    item = course['id'], course['url'], price_number(course['price']), course['title']
+    result = info_list[:]
+    result.append(item)
+    return result
+
 if __name__ == '__main__':
     count = get_response_json(1, 1)['count']
 
     PAGE_SIZE = 5
     total_pages = math.ceil(count / PAGE_SIZE)
 
-
+    course_info_list = []
     for page in range(1, total_pages + 1):
         print(str.format('---> Page # {0} of page size {1}', page, PAGE_SIZE))
 
@@ -44,10 +50,15 @@ if __name__ == '__main__':
 
         courses = data['results']
         for c in courses:
-            info = (c['id'], c['url'], c['title'], price_number(c['price']))
+            info = (c['url'], price_number(c['price']), c['title'])
             joined = u'\t'.join([str(i) for i in info])
-            # TODO save to a dsv file
-            print(joined)
+            print(str(c['id']) + ': ' + joined)
+            
+            course_info_list = append_course_info(course_info_list, c)
             print('--------------------')
             
-        # break
+        break
+
+    # print(course_info_list)
+    dataFrame = pd.DataFrame(course_info_list, columns=['id', 'url', 'price', 'title'])
+    dataFrame.to_csv('./course_info.tsv', sep='\t', index=False)
