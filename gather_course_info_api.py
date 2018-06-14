999619528d8c26ca11c347aff70e2cdabdbf580a
Hongtao Xiao <hong@Hongtaos-MacBook-Pro.local>
2018-06-13 22:10:55 -0300

    working code to retrieve basic course info for a single page

--- gather_course_info_api.py
+++ gather_course_info_api.py
@@ -13,16 +13,19 @@ def headers():
 
 if __name__ == '__main__':
 
-    parameters = {"page": 1, "page_size": 5}
+    parameters = {"page": 1, "page_size": 5, "language": 'zh'}
     
     response = requests.get(url(), params=parameters, headers=headers())
-    print(response.status_code)
-    print(response.headers)
+    # print(response.status_code)
+    # print(response.headers)
 
     # content = response.content.decode('utf-8')
     data = response.json()
     # print(type(data))
-    
-    courses = data['result']
-    print(courses)
+    # print(data)
+
+    courses = data['results']
+    for course in courses:
+        message = str.format('{0}: {1}: {2}', course['id'], course['url'], course['title'])
+        print(message)
 
