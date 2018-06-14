ec814bc5ead47163532e1262711a7489e49b9ac4
Hongtao Xiao <hong@Hongtaos-MacBook-Pro.local>
2018-06-13 22:00:52 -0300

    initial version

---
+++ gather_course_info_api.py
@@ -0,0 +1,28 @@
+import requests
+
+def url():
+    prefix = "https://www.udemy.com"
+    suffix = "/api-2.0/courses/?"
+    return prefix + suffix
+
+def headers():
+    headers = {}
+    headers['Authorization'] = 'Basic SUPER_LONG_STRING'
+    headers['Accept'] = 'application/json, text/plain, */*'
+    return headers
+
+if __name__ == '__main__':
+
+    parameters = {"page": 1, "page_size": 5}
+    
+    response = requests.get(url(), params=parameters, headers=headers())
+    print(response.status_code)
+    print(response.headers)
+
+    # content = response.content.decode('utf-8')
+    data = response.json()
+    # print(type(data))
+    
+    courses = data['result']
+    print(courses)
+
