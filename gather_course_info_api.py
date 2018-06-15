d468014f9073ff53643ac27a72fc85d799e79e7d
Hongtao Xiao <hong@hongtaos-mbp.ad.unb.ca>
2018-06-15 11:35:04 -0300

    save to file more frequently; add captured results (tsv files)

--- gather_course_info_api.py
+++ gather_course_info_api.py
@@ -18,8 +18,8 @@ def headers():
     headers['Accept'] = 'application/json, text/plain, */*'
     return headers
 
-def get_response_json(page, page_size, language = 'zh'):
-    parameters = {"page": page, "page_size": page_size, "language": language}
+def get_response_json(page, page_size, language = 'en'):
+    parameters = {"page": page, "page_size": page_size, "language": language, "ordering": 'newest'}
     response = requests.get(url(), params=parameters, headers=headers())
     return response.json()
 
@@ -35,22 +35,34 @@ def append_course_info(info_list, course):
     result.append(item)
     return result
 
+def save_to_file(course_info_list):
+    # print(course_info_list)
+    dataFrame = pd.DataFrame(course_info_list, columns=['id', 'url', 'price', 'title'])
+    # dataFrame = dataFrame.sort_values(by='id')
+    dataFrame.to_csv('./course_info.tsv', sep='\t')
+
 if __name__ == '__main__':
+    COURSE_NUMBER_LIMIT = 10000 # "page*page_size cannot be larger than 10000."
     count = get_response_json(1, 1)['count']
+    print('total count = ' + str(count))
+    count = min(count, COURSE_NUMBER_LIMIT)
 
     PAGE_SIZE = 100
     total_pages = math.ceil(count / PAGE_SIZE)
+    print(str.format('total_pages = {0} with PAGE_SIZE = {1}', total_pages, PAGE_SIZE))
 
     course_info_list = []
     for page in range(1, total_pages + 1):
+        save_to_file(course_info_list)
+
         print(str.format('---> Page # {0} of page size {1}', page, PAGE_SIZE))
 
         data = get_response_json(page, PAGE_SIZE)
 
-        if 'results' not in data:
+        while 'results' not in data:
             print('.... throttled ... wait for a few moments ....')
-            time.sleep(60)
-            continue
+            time.sleep(60 * 5)
+            data = get_response_json(page, PAGE_SIZE)
 
         courses = data['results']
         for c in courses:
@@ -60,8 +72,3 @@ if __name__ == '__main__':
             
             course_info_list = append_course_info(course_info_list, c)
             print('--------------------')
-    
-
-    # print(course_info_list)
-    dataFrame = pd.DataFrame(course_info_list, columns=['id', 'url', 'price', 'title'])
-    dataFrame.to_csv('./course_info.tsv', sep='\t')
